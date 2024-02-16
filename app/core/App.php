42da3a0426db677a9848a0bc7dba9e9f873f49f2
Gals18 <galuhsaputrii18@gmail.com>
2024-02-16 21:11:58 +0700

    controller-Method-Default

--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,42 @@
 <?php
 
 class App{
+    //membuat properti untuk controller,method, parameter
+    //agar controller dan methodnya default
+    //
+    protected $controller ='Home';
+    protected $method ='index';
+    protected $params =[];
+
     public function __construct(){
        $url = $this->parseUrl();
-       var_dump($url);
+       
+       //controller
+       if(file_exists('../app/controllers/'.$url[0].'.php')){
+           $this->controller =$url[0];
+           unset($url[0]);
+          
+       }
+
+       require_once '../app/controllers/' . $this->controller . '.php';
+       $this->controller = new $this->controller;
+
+       //method
+
+       if(isset($url[1])){
+        if(method_exists ($this->controller,$url[1]) ) {
+            $this->method = $url[1];
+            unset($url[1]);
+        }
+       }
+
+       //params
+       if(!empty($url)){
+        $this->params = array_values($url);
+       }
+
+       //jlankan controller dan method serta kirimkn params jika ada, menggunkan function  call_user_array
+       call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseUrl(){
